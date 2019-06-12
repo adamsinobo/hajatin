a624071d6adcb4cc0b058487985aa2b45856da76
Adam Wibowo <adam.wibowo@corp.go-jek.com>
2019-06-12 11:26:45 +0700

    bottom tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pagewise/flutter_pagewise.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-  debugShowCheckedModeBanner: false,
-));
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class Home extends StatefulWidget {
   @override
@@ -13,8 +14,25 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Text("Hajatin"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.insert_invitation), title: Text("Hajat")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text("Akun"))
+        ],
+      ),
     );
   }
 }
